f268a2964f6c4b2381269502ff31801983531de0
Tom Berghuis <thomas.berghuis@gmail.com>
2019-05-23 13:26:33 +1000

    .

---
+++ clone-all-remote-branches.js
@@ -0,0 +1,36 @@
+const shell = require("shelljs");
+
+const repoUrl = "https://github.com/kentcdodds/react-github-profile";
+var repoParentDir = "/home/tom/tmp/checkout-all-branches/";
+const repoName = "react-github-profile";
+
+debugger;
+
+shell.mkdir("-p", repoParentDir);
+shell.cd(repoParentDir);
+// i should probably only clone depth 1
+shell.exec(`git clone ${repoUrl} ${repoName}`);
+shell.cd(repoName);
+// probably not needed
+shell.exec("git fetch");
+
+// cant use const in debugger mode
+var branches = shell
+  .exec("git branch -a")
+  .stdout.split("\n")
+  .map(line => line.trim())
+  .filter(
+    line =>
+      line.startsWith("remotes/origin/") &&
+      !line.startsWith("remotes/origin/HEAD")
+  )
+  .map(line => line.replace("remotes/origin/", ""));
+
+branches.forEach(function(branch) {
+  shell.cd(repoParentDir);
+  shell.mkdir("-p", `branches/${branch}`);
+  shell.cd(`${repoParentDir}branches/${branch}`);
+  shell.exec(`git clone --depth 1  --branch ${branch} ${repoUrl} ${repoName}`);
+});
+
+console.log("done");
